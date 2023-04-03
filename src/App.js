1828a6c50f1db5e65ad8c105ada31d61d3aef498
maqeel019 <maqeelcs09@gmail.com>
2023-04-03 16:08:08 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
  <>
 
-    <Router>    
+    <Router basename={process.env.PUBLIC_URL}>    
       <Routes>
         <Route exact path="/" element={<Notepad />}/>        
     </Routes>
